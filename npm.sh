dc59152f20677c57e7671b25e66eb120a997d8e3
Ubuntu <ubuntu@Dhost.lxd>
2020-09-26 22:25:56 +0000

    working

---
+++ npm.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Paulo Aleixo Campos
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__dbg_on_off=off  # on off
+function shw_info { echo -e '\033[1;34m'"$1"'\033[0m'; }
+function error { echo "ERROR in ${1}"; exit 99; }
+trap 'error $LINENO' ERR
+function dbg { [[ "$__dbg_on_off" == "on" ]] || return; echo -e '\033[1;34m'"dbg $(date +%Y%m%d%H%M%S) ${BASH_LINENO[0]}\t: $@"'\033[0m';  }
+#exec > >(tee -i /tmp/$(date +%Y%m%d%H%M%S.%N)__$(basename $0).log ) 2>&1
+set -o errexit
+  # NOTE: the "trap ... ERR" alreay stops execution at any error, even when above line is commente-out
+set -o pipefail
+set -o nounset
+#set -o xtrace
+
+PATH_extend() {
+  source "${__dir}"/SOURCE_NODE_NPM_ENVVARS.source
+}
+
+main() {
+  PATH_extend
+  npm "$@"
+}
+main "$@"
+
